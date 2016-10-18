72cd52e1394bd07ca43a6ac54bfefc864855c4aa
Brad Campbell <grandstaish@gmail.com>
2016-10-18 23:25:49 +1300

    Update to PaperParcel 2.0 snapshot

--- app/src/main/kotlin/nz/bradcampbell/fourletters/redux/state/State.kt
+++ app/src/main/kotlin/nz/bradcampbell/fourletters/redux/state/State.kt
@@ -3,81 +3,96 @@ package nz.bradcampbell.fourletters.redux.state
 import android.os.Parcel
 import android.support.annotation.LayoutRes
 import nz.bradcampbell.fourletters.R
-import nz.bradcampbell.paperparcel.DefaultAdapter
-import nz.bradcampbell.paperparcel.PaperParcel
-import nz.bradcampbell.paperparcel.PaperParcelable
-import nz.bradcampbell.paperparcel.TypeAdapter
+import paperparcel.RegisterAdapter
+import paperparcel.PaperParcel
+import paperparcel.PaperParcelable
+import paperparcel.TypeAdapter
 
 @PaperParcel
 data class State(
-  val paginationState: PaginationState = PaginationState(),
-  val gameState: GameState? = null,
-  val menuState: MenuState = MenuState()
+    val paginationState: PaginationState = PaginationState(),
+    val gameState: GameState? = null,
+    val menuState: MenuState = MenuState()
 ) : PaperParcelable {
   companion object {
-    @JvmField val CREATOR = PaperParcelable.Creator(State::class.java)
+    @JvmField val CREATOR = PaperParcelState.CREATOR
   }
 }
 
 @PaperParcel
 data class PaginationState(
-  val currentPage: Page = Page(R.layout.menu),
-  val history: List<Page> = listOf<Page>()
-)
+    val currentPage: Page = Page(R.layout.menu),
+    val history: List<Page> = listOf<Page>()
+) : PaperParcelable {
+  companion object {
+    @JvmField val CREATOR = PaperParcelPaginationState.CREATOR
+  }
+}
 
 @PaperParcel
 data class GameState(
-  val answer: List<Letter> = emptyList(),
-  val leftLetter: Letter,
-  val topLetter: Letter,
-  val rightLetter: Letter,
-  val bottomLetter: Letter,
-  val possibleAnswers: List<String>,
-  val score: Int = 0,
-  val finishTime: Long
-)
+    val answer: List<Letter> = emptyList(),
+    val leftLetter: Letter,
+    val topLetter: Letter,
+    val rightLetter: Letter,
+    val bottomLetter: Letter,
+    val possibleAnswers: List<String>,
+    val score: Int = 0,
+    val finishTime: Long
+) : PaperParcelable {
+  companion object {
+    @JvmField val CREATOR = PaperParcelGameState.CREATOR
+  }
+}
 
 @PaperParcel
 data class MenuState(
-  val wordErrorDisplayed: Boolean = false
-)
+    val wordErrorDisplayed: Boolean = false
+) : PaperParcelable {
+  companion object {
+    @JvmField val CREATOR = PaperParcelMenuState.CREATOR
+  }
+}
 
 @PaperParcel
 data class Page(
-  @LayoutRes var layoutId: Int
-)
-
-enum class Position(val index: Int) {
-    LEFT(0),
-    TOP(1),
-    RIGHT(2),
-    BOTTOM(3);
-
-    companion object {
-        fun from(index: Int): Position {
-            for (position in values()) {
-                if (position.index == index) {
-                    return position
-                }
-            }
-            return LEFT
-        }
-    }
+    @LayoutRes var layoutId: Int
+) : PaperParcelable {
+  companion object {
+    @JvmField val CREATOR = PaperParcelPage.CREATOR
+  }
 }
 
 @PaperParcel
 data class Letter(
-  val position: Position,
-  val letter: Char
-)
+    val position: Position,
+    val letter: Char
+) : PaperParcelable {
+  companion object {
+    @JvmField val CREATOR = PaperParcelLetter.CREATOR
+  }
+}
 
-@DefaultAdapter
+enum class Position(val index: Int) {
+  LEFT(0),
+  TOP(1),
+  RIGHT(2),
+  BOTTOM(3);
+
+  companion object {
+    fun from(index: Int): Position {
+      return values().firstOrNull { it.index == index } ?: LEFT
+    }
+  }
+}
+
+@RegisterAdapter
 class PositionTypeAdapter : TypeAdapter<Position> {
-    override fun writeToParcel(value: Position, outParcel: Parcel, flags: Int) {
-        outParcel.writeInt(value.index)
-    }
+  override fun writeToParcel(value: Position?, dest: Parcel, flags: Int) {
+    dest.writeInt(value!!.index)
+  }
 
-    override fun readFromParcel(inParcel: Parcel): Position {
-        return Position.from(inParcel.readInt())
-    }
+  override fun readFromParcel(inParcel: Parcel): Position {
+    return Position.from(inParcel.readInt())
+  }
 }
